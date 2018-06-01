938a65690fd5dd14a49005477d89901f78a7aa93
mikhails85 <mikhails85@outlook.com>
2018-06-01 15:55:05 +0000

    add test

--- src/ES.Tests/Tests/SimpleTest.cs
+++ src/ES.Tests/Tests/SimpleTest.cs
@@ -26,32 +26,65 @@ namespace ES.Tests.Tests
         }
         
         public void RunAddToIndexTest()
-        {            
-            /*Console.WriteLine($"Try create index:'{this.Index}'");
-            CreateIndex<Employee>();            
-            Console.WriteLine($"Index created:'{this.IndexExists()}'");
-            */
-            Console.WriteLine($"Try create index:'{this.Index+"-query"}'");
-            CreatePercolatorIndex(this.Index+"-query");            
-            Console.WriteLine($"Index created:'{this.IndexExists(this.Index+"-query")}'");
+        {   
+            Console.WriteLine($"Try create index:'{this.Index}'");
+            CreatePercolatorIndex();            
+            Console.WriteLine($"Index created:'{this.IndexExists(this.Index)}'");
             
             Console.WriteLine($"Try add to index document");
             var result = this.AddToIndex(new OfferQuery{Id="1", Query = new MatchQuery 
                 {
                     Field = Infer.Field<Employee>(entry => entry.Skills),
                     Query = "skill"
-                }}, this.Index+"-query");
+                }}, this.Index);
+            
+             if (!result.IsValid)
+                Console.WriteLine($"Error: {result.DebugInformation}");
+                        
+            Console.WriteLine($"Try remove index:'{this.Index}'");
+            this.RemoveIndex(this.Index);            
+            Console.WriteLine($"Index removed:'{!this.IndexExists(this.Index)}'");            
+        }
+        
+        public void ReversSearchingTest()
+        {   
+            Console.WriteLine($"Try create index:'{this.Index}'");
+            CreatePercolatorIndex(this.Index);            
+            Console.WriteLine($"Index created:'{this.IndexExists(this.Index)}'");
+            
+            Console.WriteLine($"Try add to index document");
+            var result = this.AddToIndex(new OfferQuery{Id="1", Query = new MatchQuery 
+                {
+                    Field = Infer.Field<Employee>(entry => entry.Skills),
+                    Query = "skill"
+                }}, this.Index);
             
              if (!result.IsValid)
                 Console.WriteLine($"Error: {result.DebugInformation}");
             
-            /*Console.WriteLine($"Try remove index:'{this.Index}'");
-            this.RemoveIndex();            
-            Console.WriteLine($"Index removed:'{!this.IndexExists()}'");*/
+            Console.WriteLine($"Try search");
+            var search = Search(new Employee{Skills = new List<string>{"skill"}});
+            if (!search.IsValid)
+                Console.WriteLine($"Error: {result.DebugInformation}");            
+                
+            foreach(var doc in search.Documents)         
+            {
+                Console.WriteLine($"Doc Id: {doc.Id}");
+            }
             
-            Console.WriteLine($"Try remove index:'{this.Index+"-query"}'");
-            this.RemoveIndex(this.Index+"-query");            
-            Console.WriteLine($"Index removed:'{!this.IndexExists(this.Index+"-query")}'");            
+            Console.WriteLine($"Try search");
+            search = Search(new Employee{Skills = new List<string>{"skin"}});
+            if (!search.IsValid)
+                Console.WriteLine($"Error: {result.DebugInformation}");            
+                
+            foreach(var doc in search.Documents)         
+            {
+                Console.WriteLine($"Doc Id: {doc.Id}");
+            }
+             
+            Console.WriteLine($"Try remove index:'{this.Index}'");
+            this.RemoveIndex(this.Index);            
+            Console.WriteLine($"Index removed:'{!this.IndexExists(this.Index)}'");            
         }
         
         private void CreatePercolatorIndex(string index = null)
@@ -95,6 +128,20 @@ namespace ES.Tests.Tests
             if (!createIndexResponse.IsValid)
                 Console.WriteLine($"Error: {createIndexResponse.DebugInformation}");               
            
+        }
+        
+        private ISearchResponse<OfferQuery> Search(Employee employee)
+        {
+             var searchResponse = this.Client.Search<OfferQuery>(s => s
+                .Query(q => q
+                    .Percolate(p => p
+                        .Field(f => f.Query)
+                        .Document(employee)
+                    )
+                )
+            );
+            
+            return searchResponse;
         }
     }
 }
