1220f4eb4736feadcc6a5f135b76110913408fc7
mikhails85 <mikhails85@outlook.com>
2018-06-11 08:45:30 +0000

    commit

---
+++ src/Web.Test/Infrastructure/Domain/Contracts/IOfferManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Test.Infrastructure.Domain.Contracts
+{
+    public interface IOfferManager
+    {
+    }
+}
