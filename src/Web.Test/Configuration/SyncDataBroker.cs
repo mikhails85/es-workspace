4870f8104d0a6ee72ec56fe00035d18f13f7a00b
Mikhail Sapunov <mis@ciklum.com>
2018-07-11 13:07:28 +0300

    Add Sync DB

--- src/Web.Test/Configuration/SyncDataBroker.cs
+++ src/Web.Test/Configuration/SyncDataBroker.cs
@@ -1,17 +1,51 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Test.Infrastructure.Common.Results;
+using Web.Test.Infrastructure.Common.Wrappers;
+using Web.Test.Infrastructure.Domain.Contracts.Integration;
+using Web.Test.Infrastructure.Domain.Models;
+using Web.Test.Infrastructure.Integration.Elastic.Indexes.Queries;
+using Web.Test.Infrastructure.Integration.RabbitMQ.Handlers;
 
 namespace Web.Test.Configuration
 {
     public static class SyncDataBroker
     {
+        private static IServiceProvider provider;
         public static void UseSyncDataBroker (this IApplicationBuilder app)
         {
-            var provider = app.ApplicationServices;    
-            //TODO
+            provider = app.ApplicationServices;    
+            var employeesListener = new BatchMessageHandler<CRUDWrapper<Employee>>("employees",EmployeesBatchHandler, 10);
+            var offersListener = new BatchMessageHandler<CRUDWrapper<Offer>>("offers",OffersBatchHandler, 10);
+
+            provider.GetService<IQueueManager>().Listener(employeesListener).Start();
+            provider.GetService<IQueueManager>().Listener(offersListener).Start();
+
+            var employeesTimer = new Timer(
+                (o) => {
+                    employeesListener.FatchMessages();
+                },null,  10*1000, 10*1000);  
+
+            var offersTimer = new Timer(
+                (o) => {
+                    offersListener.FatchMessages();
+                },null,  10*1000, 10*1000);                  
+        }
+
+        private static VoidResult EmployeesBatchHandler(IEnumerable<CRUDWrapper<Employee>> batch) 
+        {
+            return provider.GetService<IESStorage>().Get<Employee>().Query(new ExecuteEmployeesBatch(batch));
+        }
+
+        private static VoidResult OffersBatchHandler(IEnumerable<CRUDWrapper<Offer>> batch) 
+        {
+            return provider.GetService<IESStorage>().Get<Offer>().Query(new ExecuteOffersBatch(batch));
         }
     }
 }
